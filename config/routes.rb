39c66d236f0386c3480cfefa949216fcff87e31f
Avery Walker <spikypancakedragon@gmail.com>
2024-04-22 14:59:05 -0500

    Add spikypancakedragon page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+
+  get 'spikypancakedragon', to: 'pages#spikypancakedragon', as: 'spikypancakedragon'
 end
